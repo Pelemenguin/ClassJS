4caa415dafdfdd4214f1a0935c29db2594ac5106
Pelemenguin <elepentuin@outlook.com>
2025-10-14 16:40:51 +0800

    Deny class `ClassJSClassLoader`

--- src/main/java/pelemenguin/classjs/ClassJSPlugin.java
+++ src/main/java/pelemenguin/classjs/ClassJSPlugin.java
@@ -5,6 +5,7 @@ import dev.latvian.mods.kubejs.script.BindingsEvent;
 import dev.latvian.mods.kubejs.script.ScriptType;
 import dev.latvian.mods.kubejs.util.ClassFilter;
 import pelemenguin.classjs.content.ClassCreator;
+import pelemenguin.classjs.util.ClassJSClassLoader;
 import pelemenguin.classjs.util.ClassJSUtils;
 import pelemenguin.classjs.util.DescriptorUtils;
 import pelemenguin.classjs.util.SignatureUtils;
@@ -28,6 +29,8 @@ public class ClassJSPlugin extends KubeJSPlugin {
         filter.allow("java.lang.Throwable");
         filter.allow("java.lang.Exception");
         filter.allow("java.lang.Error");
+
+        filter.deny(ClassJSClassLoader.class);
 
     }
 
