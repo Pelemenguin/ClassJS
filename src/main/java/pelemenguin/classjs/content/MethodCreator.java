eb943b4181130477145d8da97dc06293785fd521
Pelemenguin <elepentuin@outlook.com>
2025-10-14 22:29:51 +0800

    Fix: functions with different descriptors but a same name have the same identifier

--- src/main/java/pelemenguin/classjs/content/MethodCreator.java
+++ src/main/java/pelemenguin/classjs/content/MethodCreator.java
@@ -3604,7 +3604,7 @@ public class MethodCreator {
 
         private int anonymousFunctionCounter = 0;
         private String generateFuncId() {
-            return this.parent.parent.getClassName() + "/" + this.parent.name + "@" + (this.anonymousFunctionCounter ++);
+            return this.parent.parent.getClassName() + "/" + this.parent.name + "(" + this.parent.descriptor.hashCode() + ")@" + (this.anonymousFunctionCounter ++);
         }
 
         @Info(
